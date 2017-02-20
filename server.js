db124251a3d3991588921069bd60fa6514582419
mittino <annamefford@gmail.com>
2017-02-20 12:53:46 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static(path.join('public')));
+
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log('listening on port 3000');
+});
+
+
+module.exports = app;
