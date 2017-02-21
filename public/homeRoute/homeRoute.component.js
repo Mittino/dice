b4ab092edc7b17fd367b9ff60be4a0a956247589
mittino <annamefford@gmail.com>
2017-02-21 15:02:56 -0700

    updated home page

--- public/homeRoute/homeRoute.component.js
+++ public/homeRoute/homeRoute.component.js
@@ -8,12 +8,7 @@
   function homeRouteController($stateParams, $state){
     var vm = this;
 
-    vm.$onInit = function getAdData(){
-        console.log('init')
-      };
-
     vm.changeState = function(){
-      console.log("home route change state");
       $state.go('gameRoute');
     }
   }
